f85c464d7fb24c3a404d75cd797179723addc86a
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-09-11 19:50:02 -0400

    created function for date & time

--- src/app.js
+++ src/app.js
@@ -3,6 +3,29 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=$
 
 axios.get(apiUrl).then(displayTemperature);
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
+
 //the following function displays the temperature, city, weather description, humidity, and wind speed
 function displayTemperature(response) {
   //the following console.log helps for obtaining the source for the info from the weather API
@@ -27,4 +50,8 @@ function displayTemperature(response) {
   //this will find the wind speed from the weather API and integrate it into the app
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  //this will take the data from the formatDate function (which is milliseconds from 1970 *thats just how its calculated in JS*) and integrate it into the app
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
