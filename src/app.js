12c85dade3f674006c7019e926aeb8ef54f5e327
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-09-21 10:25:33 -0400

    moved changes from laptop, to PC

--- src/app.js
+++ src/app.js
@@ -21,10 +21,22 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
-function displayTemperature(response) {
-  //the following console.log helps for obtaining the source for the info from the weather API
-  console.log(response.data);
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayTemperature(response) {
   //this is just storing the value given by the weather API which will be used in the other function "convertToFahrenheit"
   celsiusTemperature = response.data.main.temp;
 
@@ -60,10 +72,12 @@ function displayTemperature(response) {
   );
   //this will set the alt for the icon to the description given (ie. cloudy, light rain, sunny...)
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "f43e87505c78f4b7859080149fe4a760";
+  let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -105,40 +119,43 @@ function convertToCelsius(event) {
   celsiusLink.classList.add("active");
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
-  //first, open the row
   let forecastHTML = `<div class="row">`;
-  //then define the days
-  let days = ["Thu", "Fri", "Sat"];
-  //then create a function to inject the days into the code below
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
     //then let forecastHTML equal itself (the div row from above), AND all the code written below
-    forecastHTML =
-      forecastHTML +
-      `<div class="col">
-        <div class="card">
-            <h5 class="card-header weather-forecast-date">${day}</h5>
-            <div class="card-body">
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">
+        <div class="card mt-1" id"forecast-card">
+            <h5 class="card-header weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</h5>
+            <div class="card-body" id="forecast-card-body">
               <img
-                src="http://openweathermap.org/img/wn/03n@2x.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
               />
               <p class="card-text weather-forecast-temperatures">
                 <span class="weather-forecast-temperatures-max"
-                  >22°
+                  >${Math.round(forecastDay.temp.max)}°
                 </span>
                 <span class="weather-forecast-temperatures-min"
-                  >18°</span
+                  >${Math.round(forecastDay.temp.min)}°</span
                  >
               </p>
             </div>
           </div>
         </div>`;
+    }
   });
+
   //then close the row
   forecastHTML = forecastHTML + `</div>`;
   //then inject it into the HTML
   forecastElement.innerHTML = forecastHTML;
 }
-displayForecast();
